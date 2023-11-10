69c50b7b3faf8d06ecce1f5a2c9a4382f6b3e663
Ege Özel <ege.ozel@paratica.com>
2023-11-11 01:54:01 +0300

    Mobile sign in page design for the first register page (Register A) is implemented.

--- src/App.js
+++ src/App.js
@@ -1,70 +1,21 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Register from "./Register";
-import { ThemeProvider, createTheme } from "@mui/material";
+import RegisterA from "./registerPages/RegisterA";
 
 const COLOR_PRIMARY = "#39434F";
 const COLOR_WHITE = "#C6CED9";
-const COLOR_SECONDARY = "rgb(158, 124, 186)";
-const COLOR_TERNARY = "#6152AD";
-const COLOR_DARK = "#222222";
+const COLOR_SECONDARY = "#779341";
+const COLOR_FADE = "#8D8D8D";
+const COLOR_DARK = "#000000";
 
 const theme = createTheme({
-  typography: {
-    fontFamily: "Noto Sans",
-    fontWeight: 500,
-    allVariants: {
-      color: COLOR_PRIMARY,
-      fontSize: "16px",
-      textTransform: "none",
-    },
-  },
-
   components: {
-    MuiTextField: {
-      styleOverrides: {
-        root: { backgroundColor: "#FFFFFF" },
-        input: {
-          color: COLOR_PRIMARY,
-        },
-      },
-    },
-    MuiInputLabel: {
-      styleOverrides: {
-        root: {
-          color: COLOR_DARK,
-        },
-      },
-    },
-    MuiOutlinedInput: {
-      styleOverrides: {
-        root: {
-          backgroundColor: "#FFFFFF",
-        },
-      },
-    },
-    palette: {
-      primary: {
-        main: COLOR_PRIMARY,
-      },
-      secondary: {
-        main: COLOR_SECONDARY,
-      },
-      ternary: {
-        main: COLOR_TERNARY,
-      },
-      text: {
-        primary: COLOR_DARK,
-        secondary: COLOR_WHITE,
-        dark: COLOR_DARK,
-      },
-    },
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          borderRadius: 8,
-          textTransform: "none",
-        },
+    MuiTypography: {
+      defaultProps: {
+        fontFamily: "Poppins",
+        color: "#000000",
+        textTransform: "none",
       },
     },
   },
@@ -75,7 +26,7 @@ function App() {
     <div className="App">
       <ThemeProvider theme={theme}>
         <Routes>
-          <Route path="/" element={<Register />}></Route>
+          <Route path="/" element={<RegisterA />}></Route>
         </Routes>
       </ThemeProvider>
     </div>
---
+++ src/images/index.js
@@ -0,0 +1,16 @@
+export {
+  default as mobileBackground,
+  ReactComponent as MobileBackground,
+} from "./background-a-mobile.svg";
+export {
+  default as mobileRectangle,
+  ReactComponent as MobileRectangle,
+} from "./mobile-rectangle.svg";
+export {
+  default as loginGoogle,
+  ReactComponent as LoginGoogle,
+} from "./login-google.svg";
+export {
+  default as loginFacebook,
+  ReactComponent as LoginFacebook,
+} from "./login-facebook.svg";
