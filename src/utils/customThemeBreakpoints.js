e65bf4c4f38d763c7a2adbf169566f13c3fa3a68
Ege Özel <ege.ozel@paratica.com>
2023-11-21 19:52:52 +0300

    Login page responsiveness implemented and register page positionings is corrected.

---
+++ src/utils/customThemeBreakpoints.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 500,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
